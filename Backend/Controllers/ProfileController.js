0014d75ec7fcfe271143bf92ee13bb63b9bd578d
Damian Wujec <wujecdamian12@gmail.com>
2026-07-20 16:20:34 +0200

    feat: add user validation to editProfile to restrict profile edits

--- Backend/Controllers/ProfileController.js
+++ Backend/Controllers/ProfileController.js
@@ -14,7 +14,12 @@ const getProfileById = async (req, res) => {
 
 const editProfile = async (req, res) => {
   const { bannerPhotoUrl, profilePhotoUrl };
+  const user = req.user;
   const userId = req.params.userId;
+
+  if (user.id !== userId) {
+    res.json({ errorMessage: "You can edit only your own profile" });
+  }
 
   const groups = await prisma.group.update({
     where: { id: Number(userId) },
