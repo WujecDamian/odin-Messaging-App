dfd71e5e52a6d5cdfa13f9a4d475dc73315440a5
Damian Wujec <wujecdamian12@gmail.com>
2026-07-21 00:22:01 +0200

    fix: fix form values undefined (get values form bodyData)
    + check if values are not undefined

--- Backend/Controllers/RegisterController.js
+++ Backend/Controllers/RegisterController.js
@@ -1,19 +1,29 @@
 import bcrypt from "bcryptjs";
 import { prisma } from "../../lib/prisma.js";
+import { error } from "node:console";
 
 const createUser = async (req, res) => {
-  const { username, password, displayName } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
+  const { username, password, displayName } = req.body.bodyData;
+  if (!username || !password || !displayName) {
+    return res
+      .status(400)
+      .json({ error: "You need to pass username and password." });
+  }
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-  const newUser = await prisma.user.create({
-    data: {
-      username,
-      password: hashedPassword,
-      displayName,
-    },
-  });
-
-  res.json({ newUser });
+    const newUser = await prisma.user.create({
+      data: {
+        username: username,
+        password: hashedPassword,
+        displayName: displayName,
+      },
+    });
+    res.status(201).json({ newUser });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error while creating user." });
+  }
 };
 
 export { createUser };
