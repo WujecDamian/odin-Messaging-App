f5a9c79e0c36dce02b9c5107c5a2150fa9d6a4ed
Damian Wujec <wujecdamian12@gmail.com>
2026-07-21 03:50:07 +0200

    feat: update Navbar to conditionally render login/logout links based on user authentication

--- Frontend/src/Components/Navbar/Navbar.jsx
+++ Frontend/src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,27 @@
 import { Link } from "react-router-dom";
 import styles from "./Navbar.module.css";
+import { useCookies } from "react-cookie";
 
 function Navbar() {
+  const [cookies] = useCookies(["user", "token"]);
+
+  const token = cookies.token;
+  const user = cookies.user;
+  if (user) {
+    console.log("User is logged in! ", user);
+  }
   return (
     <>
       <nav className={styles.navbar}>
         <Link to="/">Home</Link>
-        <Link to="/register">Register</Link>
-        <Link to="/login">Login</Link>
+        {user ? (
+          <Link to="/logout">Log out</Link>
+        ) : (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
       </nav>
     </>
   );
