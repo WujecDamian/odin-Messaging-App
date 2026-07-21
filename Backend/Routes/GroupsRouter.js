35825a2512f4367f334d49fb2617e11e1156a255
Damian Wujec <wujecdamian12@gmail.com>
2026-07-21 14:28:59 +0200

    feat: add verifyToken middleware to addUserToGroup POST route

--- Backend/Routes/GroupsRouter.js
+++ Backend/Routes/GroupsRouter.js
@@ -3,12 +3,13 @@ import express from "express";
 const router = express.Router();
 
 import * as GroupsController from "../Controllers/GroupsController.js";
+import verifyToken from "../Middleware/authMiddleware.js";
 
 // define the home page route
 router.get("/", GroupsController.getAllGroups);
 
 router.get("/group/:groupId", GroupsController.getGroupById);
 
-router.post("/group/:groupId", GroupsController.addUserToGroup);
+router.post("/group/:groupId", verifyToken, GroupsController.addUserToGroup);
 
 export default router;
